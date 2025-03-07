cd64337f08e327c0bd6c9852ed92d293e80dc104
NadineMlayeh <nadine.mlayah@gmail.com>
2025-03-07 16:17:45 +0100

    update status

---
+++ app.py.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, CI/CD!"
+
+# New status route
+@app.route('/status')
+def status():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run()
